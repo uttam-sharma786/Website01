1579b32230274415e9245baeffb5da5a328634e9
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-05-15 12:25:51 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-3xl text-green-300 bg-black'> hi</h1>
+     
+    </>
+  )
+}
+
+export default App
